23e7f74f0095756c4238bda5695b8f2e8d6a9a23
Joel Watkins <eck087@yahoo.co.uk>
2022-11-15 14:20:35 +0000

    colour rating finished

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,30 @@
 import { useState } from "react";
 
+const initialFormState = {
+  colourRating: "",
+  spendTime: "",
+  comments: "",
+  name: "",
+  email: "",
+};
+
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [formState, setFormState] = useState(initialFormState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+    setFormState(initialFormState);
+  };
+
+  const handleChange = (e) => {
+    const targetName = e.target.name;
+    const targetValue = e.target.value;
+    if (targetName === "color") {
+      setFormState({ ...formState, colourRating: targetValue });
+    }
+  };
 
   return (
     <main className="main">
@@ -9,7 +32,102 @@ function Main() {
         <h2>Answers list</h2>
         {/* answers should go here */}
       </section>
-      <section className="main__form">{/* a form should be here */}</section>
+      <section className="main__form">
+        <form class="form" onSubmit={handleSubmit}>
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div class="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <ul>
+              <li>
+                <input
+                  id="color-one"
+                  type="radio"
+                  name="color"
+                  value="1"
+                  checked={formState.colourRating === "1"}
+                  onChange={handleChange}
+                />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input
+                  id="color-two"
+                  type="radio"
+                  name="color"
+                  value="2"
+                  checked={formState.colourRating === "2"}
+                  onChange={handleChange}
+                />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input
+                  id="color-three"
+                  type="radio"
+                  name="color"
+                  value="3"
+                  checked={formState.colourRating === "3"}
+                  onChange={handleChange}
+                />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input
+                  id="color-four"
+                  type="radio"
+                  name="color"
+                  value="4"
+                  checked={formState.colourRating === "4"}
+                  onChange={handleChange}
+                />
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+          </div>
+          <div class="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <ul>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="swimming" />
+                  Swimming
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="bathing" />
+                  Bathing
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="chatting" />
+                  Chatting
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="noTime" />I
+                  don't like to spend time with it
+                </label>
+              </li>
+            </ul>
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea name="review" cols="30" rows="10"></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input type="text" name="username" value="" />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input type="email" name="email" value="" />
+          </label>
+          <input class="form__submit" type="submit" value="Submit Survey!" />
+        </form>
+      </section>
     </main>
   );
 }
